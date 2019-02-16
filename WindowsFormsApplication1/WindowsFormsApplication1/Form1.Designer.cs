f81e751d82fe3d0173b0d901fca8ef20c0cd3879
Полина <markina_pln@mail.ru>
2019-02-16 21:09:27 +0300

    CodeImage

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Закодировать изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Вывести изображение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 413);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 31);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(183, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 328);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 60);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Раскодировать файл ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(293, 388);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 39);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Удалить изображение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(21, 313);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 60);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Сравнить произвольные изображения";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(658, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(384, 328);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(793, 388);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(140, 39);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Удалить изображение";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(573, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(79, 24);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Сравнить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Исходное изображение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(768, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Декодированное изображение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 469);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Кодирование изображений с использованием алгоритма группового сжатия";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        bool pictlod = false;
+        string filename;
+        byte[] ByteArray;
+        byte[] ByteArray2;
+        long sizeofimage,sizeofimage2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+        
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.DefaultExt = "*.TIFF";
+            openFileDialog1.Filter = "Файлы TIFF (*.tif)|*.tif";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+
+                FileStream fstream = File.OpenRead(openFileDialog1.FileName);
+
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                filename = openFileDialog1.FileName;
+                ByteArray = new byte[fstream.Length];    //создали массив размеров с файл , загрузили туда байты
+                fstream.Read(ByteArray, 0, ByteArray.Length);
+                fstream.Dispose();
+                pictlod = true;
+            }
+            else {
+                pictlod = false;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (pictlod == false)
+            {
+
+                MessageBox.Show("Необходимо загрузить изображение!");
+                return;
+            }
+
+
+
+            string path = "";
+
+            if (path.Length == 0)
+            {
+                SaveFileDialog save = new SaveFileDialog();
+
+                save.DefaultExt = "*.POLPAL";
+                save.Filter = "Файлы POLPAL (*.pol)|*.pol";
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    path = save.FileName; //тут просто диалоговое окно открывалось для названия файла
+
+                }
+
+                else
+                {
+                    return;
+                }
+            }
+            
+
+
+            FileStream fstream = new FileStream(path, FileMode.OpenOrCreate); //создали поток файла
+            BinaryWriter compr = new BinaryWriter(fstream);
+
+
+
+            
+
+ 
+            long i = 0;
+            int povt = 1;
+            byte cv;
+
+           sizeofimage = ByteArray.Length;
+
+
+            while (i < sizeofimage-1) //открываем цикл ,будем все делать до тех пор пока массив не кончится
+            {
+               
+
+                    povt = 1;
+
+                if ((i + 3)> (sizeofimage - 1)) // если мы пришли к концу файла и у нас осталось
+                                                     //2 байта,конечно мы их не будем сравнивать со следующими ,у нас конец массива
+                {
+                    if (ByteArray[i] < 0xC0)
+                    {
+                        compr.Write(ByteArray[i]);
+                        compr.Write(ByteArray[i + 1]);         //кароч если байт меньше С0 то пишем его таким какой он есть  (неповторяющийся байт)               
+                        break;
+                    }
+
+                    else
+                    {
+
+
+                        cv = Convert.ToByte(0xC1);
+                        compr.Write(cv);
+                        compr.Write(ByteArray[i]); // если байт больше С0,то пишем С1-значение байта повторителя+значение байта
+                        compr.Write(ByteArray[i + 1]);
+                        break;
+
+
+                    }
+
+
+                }
+
+                    if (ByteArray[i] == ByteArray[i + 2] & ByteArray[i + 1] == ByteArray[i + 3]) //если байты повторяются
+                    {
+
+                        while (ByteArray[i] == ByteArray[i + 2] & ByteArray[i + 1] == ByteArray[i + 3])
+                        {     //тут уже все интереснее,мы сравниваем байт со следующим байтом ,если одинаковые стакуем повторения
+
+                            povt++;
+                            i = i + 2;
+
+                        
+
+                            if (povt == 63) //помним ,что повторения нельзя превышать 64,настаковали 63,обнулили группу
+                            {
+                                cv = Convert.ToByte(povt + 0xC0);
+                                compr.Write(cv);
+                                compr.Write(ByteArray[i]);
+                                compr.Write(ByteArray[i + 1]);
+                                povt = 1;
+                                i = i + 2;
+
+                            }
+                        }
+
+
+                        cv = Convert.ToByte(povt + 0xC0); //тут все пишем норм ,если повторений не много (cv-счетчик)
+                        compr.Write(cv);
+                        compr.Write(ByteArray[i]);
+                        compr.Write(ByteArray[i + 1]);
+                        povt = 1;
+                        i = i + 2;
+                    }
+                
+
+                else //если байты одиночны
+                {
+
+                    if (ByteArray[i] < 0xC0)
+                    {
+                        compr.Write(ByteArray[i]);
+                        compr.Write(ByteArray[i + 1]);         //кароч если байт меньше С0 то пишем его таким какой он есть  (неповторяющийся байт)               
+                        i = i + 2;
+                        povt = 1;
+                    }
+
+                    else
+                    {
+
+
+                        cv = Convert.ToByte(0xC1);
+                        compr.Write(cv);
+                        compr.Write(ByteArray[i]); // если байт больше С0,то пишем С1-значение байта повторителя+значение байта
+                        compr.Write(ByteArray[i + 1]);
+                        i = i + 2;
+                        povt = 1;
+
+
+                    }
+                }
+                
+            }
+
+            fstream.Close();
+            compr.Close();
+
+
+        }
+
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.DefaultExt = "*.POLPAL";
+            openFileDialog1.Filter = "Файлы POLPAL (*.pol)|*.pol";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+
+                FileStream fstream = File.OpenRead(openFileDialog1.FileName);
+                filename = openFileDialog1.FileName;
+                ByteArray2 = new byte[fstream.Length];    //создали массив размеров с файл , загрузили туда байты
+                fstream.Read(ByteArray2, 0, ByteArray2.Length);
+                fstream.Dispose();
+                pictlod = true;
+            }
+            else
+            {
+                return;
+            }
+
+
+
+                string path = "";
+
+                if (path.Length == 0)
+                {
+                    SaveFileDialog save = new SaveFileDialog();
+
+                    save.DefaultExt = "*.TIFF";
+                    save.Filter = "Файлы TIFF (*.tif)|*.tif";
+
+                    if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        path = save.FileName; //тут просто диалоговое окно открывалось для названия файла
+
+                    }
+
+                    else
+                    {
+                        return;
+                    }
+                }
+
+
+
+                FileStream fstream2 = new FileStream(path, FileMode.OpenOrCreate); //создали поток файла
+                BinaryWriter compr2 = new BinaryWriter(fstream2);
+
+
+                long i = 0;
+                int povt = 0;
+
+                sizeofimage2 = ByteArray2.Length;
+                byte[] BufArray = new byte[2] ; //2 элемента 
+
+                while (i < sizeofimage2 - 1) //открываем цикл ,будем все делать до тех пор пока массив не кончится
+                {
+
+
+
+                    
+                        if (ByteArray2[i] < 0xC0)
+                        {
+                            compr2.Write(ByteArray2[i]);
+                            compr2.Write(ByteArray2[i + 1]);         //кароч если байт меньше С0 то пишем его таким какой он есть  (неповторяющийся байт)               
+                            i = i + 2;
+                        }
+
+                        else
+                        {
+
+                            povt = ByteArray2[i] - 0xC0;
+
+                              BufArray[0] = ByteArray2[i + 1];
+                              BufArray[1] = ByteArray2[i + 2];
+
+
+                        for (int j = povt; j >= 1; j--)
+                        {
+
+                            compr2.Write(BufArray[0]);
+                            compr2.Write(BufArray[1]);
+
+                        }
+
+                    BufArray[0] = 0;
+                    BufArray[1] = 0;
+
+                        povt = 0;
+                        i = i + 3;
+
+                        }
+
+
+                }
+
+
+                fstream2.Close();
+                compr2.Close();
+
+
+
+            pictureBox2.Image = Image.FromFile(path);
+            
+            FileStream fstream3 = File.OpenRead(path); //осторожно тут жесть
+            ByteArray2 = new byte[fstream3.Length];
+            sizeofimage2 = ByteArray2.Length;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+                pictlod = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            byte[] ByteSrav;
+            byte[] ByteSrav2;
+            long sizeofimage1;
+            long sizeofimage3;
+
+            
+                var openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.DefaultExt = "*.TIFF";
+                openFileDialog1.Filter = "Файлы TIFF (*.tif)|*.tif";
+
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+
+
+                    FileStream fstream = File.OpenRead(openFileDialog1.FileName);
+
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    filename = openFileDialog1.FileName;
+                    ByteSrav = new byte[fstream.Length];
+                    sizeofimage1 = ByteSrav.Length;
+                     
+
+                }
+                else
+                {
+                    return;
+                }
+            
+
+            var openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.DefaultExt = "*.TIFF";
+            openFileDialog2.Filter = "Файлы TIFF (*.tif)|*.tif";
+
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+
+                FileStream fstream = File.OpenRead(openFileDialog2.FileName);
+
+                pictureBox2.Image = Image.FromFile(openFileDialog2.FileName);
+                filename = openFileDialog2.FileName;
+                ByteSrav2 = new byte[fstream.Length];
+                sizeofimage3 = ByteSrav2.Length;
+            }
+            else
+            {
+                return;
+            }
+
+
+
+
+
+
+            if (sizeofimage1 == sizeofimage3)
+                MessageBox.Show("Размер файлов одинаков!");
+            else
+                MessageBox.Show("Размер файлов не совпадает!");
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image != null & pictureBox2.Image != null)
+            {
+                if (sizeofimage == sizeofimage2)
+                    MessageBox.Show("Размер файлов одинаков!");
+                else
+                    MessageBox.Show("Размер файлов не совпадает!");
+            }
+            else
+            {
+
+                if (pictureBox1.Image == null & pictureBox1.Image == null)
+                    MessageBox.Show("Загрузите изображения !");
+                else
+
+                if (pictureBox1.Image == null)
+                    MessageBox.Show("Загрузите исходное изображение!");
+                else
+                     if (pictureBox2.Image == null)
+                    MessageBox.Show("Загрузите изображение для декодирования!");
+              
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                pictureBox2.Image.Dispose();
+                pictureBox2.Image = null;
+                
+            }
+        }
+    }
+    }
+
